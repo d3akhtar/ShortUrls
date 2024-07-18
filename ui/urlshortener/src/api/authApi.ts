48e3fb024e6950c86a849aaf5c005bd5da34455c
d3akhtar <danyal.akhtar@torontomu.ca>
2024-07-18 12:47:14 -0400

    forgot to update auth api link to https

--- ui/urlshortener/src/api/authApi.ts
+++ ui/urlshortener/src/api/authApi.ts
@@ -3,7 +3,7 @@ import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
     reducerPath: "authApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://shorturls.danyalakt.com/auth/"
+        baseUrl: "https://shorturls.danyalakt.com/auth/"
     }),
     endpoints: (builder) => ({
        login: builder.mutation({
